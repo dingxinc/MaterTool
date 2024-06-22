c5f71af597d54ce0e056f06a6c1befc35e6c9b04
mater <754888460@qq.com>
2024-06-22 21:25:12 +0800

    增加代码注释

--- tool/Any.h
+++ tool/Any.h
@@ -39,7 +39,7 @@ namespace mater
 
 		template <typename T>
 		struct type_impl : public type_base {
-			T data_;
+			T data_;    // 数据成员，通过T传入具体的类型，完成类型擦除
 
 			explicit type_impl(const T& data)
 				: data_(data)
@@ -51,6 +51,7 @@ namespace mater
 			{
 			}
 
+			/* 可变参数的移动构造 */
 			template<class... Args>
 			explicit type_impl(Args&&... args)
 				: data_(std::forward<Args>(args)...)
@@ -64,6 +65,7 @@ namespace mater
 
 			auto clone() const -> std::unique_ptr<type_base> override
 			{
+				/* 用父类指针接受子类构造出来的对象 */
 				return std::unique_ptr<type_impl>(new type_impl(data_));
 			}
 		};
@@ -99,6 +101,7 @@ namespace mater
 				return *this;
 			}
 
+			/* decay的作用是去掉 T 的const 和 & ，让类似于 const T& 直接变成 T */
 			template<class T>
 			using decay = typename std::decay<T>::type;
 			template<class T, typename std::enable_if<!std::is_same<decay<T>, any>::value, bool>::type = true>
@@ -114,11 +117,12 @@ namespace mater
 				return *this;
 			}
 
-			auto has_value() const -> bool
+			auto has_value() const -> bool // 返回 m_data对象 是否为真，即是否有 m_data 这个对象
 			{
 				return m_data.operator bool();
 			}
 
+			// 返回 m_data 的实际类型，其实是 T 传进来的，这是一个虚函数的多态
 			auto type() const -> const std::type_info&
 			{
 				return has_value() ? m_data->get_type() : typeid(void);
@@ -136,7 +140,7 @@ namespace mater
 
 			template<class T, class... Args>
 			void emplace(Args&&... args)
-			{
+			{//m_data 智能指针指向 reset(new object) 里的新对象
 				m_data.reset(new type_impl<decay<T>>(std::forward<Args>(args)...));
 			}
 
@@ -157,6 +161,7 @@ namespace mater
 			}
 
 		private:
+			/* 检查类型是否一致 */
 		    template<class T>
 		    auto check_type() const -> bool
 		    {
@@ -168,6 +173,7 @@ namespace mater
 			    return true;
 		    }
 
+			/* 检查数据成员是否存在 */
 		    auto check_not_null() const -> bool
 		    {
 			    if (!m_data)
